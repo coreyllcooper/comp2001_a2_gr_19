9eb6a0df1a8521736c5eef7da4c16a3378cc0264
Leonard O'Neill <k74lson@mun.ca>
2021-10-12 18:18:04 -0230

    Finished the Registration constructor

--- Registration.java
+++ Registration.java
@@ -20,29 +20,72 @@ public class Registration
     public Registration(String courseNumber, int section, int slot, 
                       String student,String professor)
     {
-        // initialise instance variables
+        if (this.verifyCourseNumber(courseNumber)){
+            this.courseNumber = courseNumber;
+        } else{
+            System.out.print("Invalid Course Number");
+            courseNumber = "INVALID";
+        }
+        
+        if (this.verifySection(section)){
+            this.section = section;
+        } else{
+            System.out.print("Invalid Section");
+            this.section = 3;
+        }
+        
+        if (this.verifySlot(slot)){
+            this.timeSlot = slot;
+        } else{
+            System.out.print("Invalid Time Slot");
+            this.timeSlot = 0;
+        }
+        
+        this.student = student;
+        
+        this.professor = professor;
+            
+        
+        
+       
         
     }
     
     private boolean verifyCourseNumber(String courseNumber)
     {
-        return true;
+        String[] courses = {"COMP 1501", "ENGL 2201", "CHEM 3060", "PHYS 2344", "ENGL 2005", "COMP 2704", "PHYS 2377", "CHEM 4927", "COMP 3444"};
+        boolean isVerified = false;
+        for(String course : courses){
+         if (courseNumber.equals(course)){
+                isVerified = true;
+            }   
+        }
+        return isVerified;
     }
     
     private boolean verifySection(int section)
-    {
-        return true;
+    {   boolean isVerified = false;
+        if (section >= 0 && section <=2) {
+        isVerified = true;
+    }
+    return isVerified;
     }
     
     private boolean verifySlot (int slot)
-    {
-        return true;
+    {   boolean isVerified = false;
+        if (slot >=12 && slot <=25){
+        isVerified = true;
+    }
+    return isVerified;
     }
 
    public void printInfo()
    {
-       // print all the information about the enrollment record
-       // each field on a new line
+        System.out.println("Course number: " + courseNumber);
+        System.out.println("Section: " + section);
+        System.out.println("Time Slot: " + timeSlot);
+        System.out.println("Student: " + student);
+        System.out.println("Professor: " + professor);
    }
     
 }
