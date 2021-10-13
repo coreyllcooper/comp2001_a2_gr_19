6371570721d519082e157da3d66c84ecbfff5f9a
Len <lencompsci@gmail.com>
2021-10-13 14:09:14 -0230

    finished the logic for the populateRegistrations() method

--- CollegeRegistrar.java
+++ CollegeRegistrar.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.*;
+import java.util.Random;
+
 
 /**
  * Write a description of class CollegeRegistrar here.
@@ -10,8 +11,6 @@ import java.util.*;
 public class CollegeRegistrar
 {
     private ArrayList<Registration> courseRegistrations = new ArrayList<>();
-    
-    
 
 
     /**
@@ -19,17 +18,36 @@ public class CollegeRegistrar
      */
     public CollegeRegistrar()
     {
-       //These are just demo Registration objects that are instantiated with the class so we can test our methods. We can remove this later.
-        
+        //These are just demo Registration objects that are instantiated with the class so we can test our methods. We can remove this later.
+
         Registration LenComp = new Registration("COMP 1501",1,17,"Len","Fiech");
-       Registration AdamComp = new Registration("COMP 2704",0,20,"Adam","Xiang");
-       Registration CoreyComp = new Registration("COMP 3444",2,21,"Corey","Kolokolova");
-       courseRegistrations.add(LenComp);
-       courseRegistrations.add(AdamComp);
-       courseRegistrations.add(CoreyComp);
-        
-           
+        Registration AdamComp = new Registration("COMP 2704",0,20,"Adam","Xiang");
+        Registration CoreyComp = new Registration("COMP 3444",2,21,"Corey","Kolokolova");
+        courseRegistrations.add(LenComp);
+        courseRegistrations.add(AdamComp);
+        courseRegistrations.add(CoreyComp);
+
        
+    }
+    public void populateRegistrations(){
+        
+        
+        
+        String[] courses = {"COMP 1501", "ENGL 2201", "CHEM 3060", "PHYS 2344", "ENGL 2005", "COMP 2704", "PHYS 2377", "CHEM 4927", "COMP 3444"};
+        String[] names = {"Nora", "Manpreet", "Ila","Jacob","Abhinav","Joanna","Patti","Avineet","Fen","Esteban","Nina","Braeden","Chhamandeep","Michael","Chelsea","Baozhai"};
+        String[] profs = {"Fiech", "Xiang","Kolokolova","Wareham","Tang","Bazhaf","Mata-Montero","Miminis","Gupta"};
+        for(int i = 0; i < 30; i++){
+            int randomSlot = (int)Math.floor(Math.random()*(25-12+1)+12);
+            int randomSection = (int)Math.floor(Math.random()*(2-0+1)+0);
+            int randomCourseIndex = (int)Math.floor(Math.random()*(courses.length-1));
+            int randomNameIndex = (int)Math.floor(Math.random()*(names.length-1));
+            int randomProfIndex = (int)Math.floor(Math.random()*(profs.length-1));
+            Registration tempRegistration = new Registration(courses[randomCourseIndex],randomSection,randomSlot,names[randomNameIndex],profs[randomProfIndex]);
+            courseRegistrations.add(tempRegistration);
+            
+            
+        }
+        
     }
 
     /**
@@ -40,94 +58,98 @@ public class CollegeRegistrar
      */
     public void addRegistration(Registration newRegistration)
     {
-       courseRegistrations.add(newRegistration);
-        
+        courseRegistrations.add(newRegistration);
+
     }
-    
+
     public void printAllRegistrations()
     {
         // prints for all registrations the corresponding info
         // registration records are separated by an empty line
         courseRegistrations.stream()
-            .forEach((registration) ->
+        .forEach((registration) ->
             {
                 registration.printInfo();
             }
-            );
-        
+        );
+
     }
-    
+
     public void printClassesForStudent(String student)
     {
         // prints all classes (course number and professor) in which the 
         // student is enrolled
         // one class per line
-        
+
         //courseRegistrations.stream()    // creates a stream of student registration
         //.filter(registration -> registration.getStudent().equals(student)) // loops over each courseNumber in stream
         //.forEach(registration -> registration.printInfo()); // prints courseNumber and professor for all courses
+
+        courseRegistrations.stream() // creates a stream of registered classes
+        .filter(registration -> registration.getStudent().equals(student)) // filters by student name
+        .forEach(registration -> registration.printInfo()); // prints info about each class in the stream
+
     }
-    
+
     public int getClassCountForStudent(String student)
     {
         // number of classes in which the student is enrolled
-        
+
         //return (int)courseRegistrations.stream()
         //.filter(registration -> registration.getStudent().equals(student))
         //.count();  
-        
+
+        return (int)courseRegistrations.stream() // creates a stream of registered classes
+        .filter(registration -> registration.getStudent().equals(student)) // filters by student name
+        .count(); // counts the number of classes in the stream
+
     }
-    
-    
+
     public void printClassesForStudentByProfessor(String student, String professor)
     {
         // prints all classes (course number and section) 
         // tought by professor and taken by student
         // one class per line
-        
-        //student.stream()    // creates a stream of student registration
-        //.filter(student -> professor.equals(student.professor))  // adds courses by given prof
-        //.forEach(courseNumber ->     // loops over each courseNumber in stream
-        //    System.out.println("Class: " + courseNumber + ". Section: " + section)  // prints courseNumber and section for all courses
-        //);
+        courseRegistrations.stream()    // creates a stream of registered classes
+        .filter(registration -> registration.getStudent().equals(student)) // filters by student name
+        .filter(registration -> registration.getProfessor().equals(professor)) // filters by professor name
+        .forEach(registration -> registration.printInfo()); // prints info about each class in the stream
     }
-    
+
     public void printClassesForStudentInSlot(String student, int timeSlot)
     {
         // prints all classes (course number and section)
         // taken by student in timeSlot
         // one class per line
-        
-        //student.stream()    // creates a stream of student registration
-        //.filter(student -> timeSlot == student.timeSlot     // adds courses by given timeSlot
-        //.forEach(courseNumber ->    // loops over each courseNumber in stream
-        //    System.out.println("Class: " + courseNumber + ". Section: " + section))     // prints courseNumber and section for all courses
-        //);
+        courseRegistrations.stream() // creates a stream of registered classes
+        .filter(registration -> registration.getStudent().equals(student)) // filters by student name
+        .filter(registration -> registration.getTimeSlot() == timeSlot) // filters by professor name
+        .forEach(registration -> registration.printInfo()); // prints info about each class in the stream
     }
-    
+
     public int getRegistrationsInClass(String classNumber)
     {
         // get the number of students registered in classNumber
         return 0;
     }
-    
+
     public int getRegistrationsInClass(String classNumber, int timeSlot)
     {
         // get the number of students enrolled in classNumber and slot
         return 0;
     }
-    
+
     public int studentsTakingClassesInSlot(int timeSlot)
     {
         // get the number of students taking classes in timeSlot
         return 0;
     }
-    
+
     public void dropClassForStudent(String classNumber, String student)
     {
         // drops class classNumber for student
     }
-    
+
     public void dropAllClassesForStudent(String student)
     {
         // drops all classes for student
--- Registration.java
+++ Registration.java
@@ -18,7 +18,7 @@ public class Registration
      * Constructor for objects of class Registration
      */
     public Registration(String courseNumber, int section, int slot, 
-                      String student,String professor)
+    String student,String professor)
     {
         if (this.verifyCourseNumber(courseNumber)){
             this.courseNumber = courseNumber;
@@ -26,72 +26,75 @@ public class Registration
             System.out.print("Invalid Course Number");
             courseNumber = "INVALID";
         }
-        
+
         if (this.verifySection(section)){
             this.section = section;
         } else{
             System.out.print("Invalid Section");
             this.section = 3;
         }
-        
+
         if (this.verifySlot(slot)){
             this.timeSlot = slot;
         } else{
             System.out.print("Invalid Time Slot");
             this.timeSlot = 0;
         }
-        
+
         this.student = student;
-        
+
         this.professor = professor;
-            
+
         
-        
-       
         
     }
-    
+
     private boolean verifyCourseNumber(String courseNumber)
     {
         String[] courses = {"COMP 1501", "ENGL 2201", "CHEM 3060", "PHYS 2344", "ENGL 2005", "COMP 2704", "PHYS 2377", "CHEM 4927", "COMP 3444"};
         boolean isVerified = false;
         for(String course : courses){
-         if (courseNumber.equals(course)){
+            if (courseNumber.equals(course)){
                 isVerified = true;
             }   
         }
         return isVerified;
     }
-    
+
     private boolean verifySection(int section)
     {   boolean isVerified = false;
         if (section >= 0 && section <=2) {
-        isVerified = true;
+            isVerified = true;
+        }
+        return isVerified;
     }
-    return isVerified;
-    }
-    
+
     private boolean verifySlot (int slot)
     {   boolean isVerified = false;
         if (slot >=12 && slot <=25){
-        isVerified = true;
-    }
-    return isVerified;
+            isVerified = true;
+        }
+        return isVerified;
     }
 
-   public void printInfo()
-   {
+    public void printInfo()
+    {
         System.out.println("Course number: " + courseNumber);
         System.out.println("Section: " + section);
         System.out.println("Time Slot: " + timeSlot);
         System.out.println("Student: " + student);
         System.out.println("Professor: " + professor);
-   }
-    
-   public String getStudent(){
-       return this.student;
-   }
-   public String getProfessor(){
-       return this.professor;
-   }
+    }
+
+    public String getStudent(){
+        return this.student;
+    }
+
+    public String getProfessor(){
+        return this.professor;
+    }
+
+    public int getTimeSlot(){
+        return this.timeSlot;
+    }
 }
